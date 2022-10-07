7c1950eb0ee26e52bbca26f1d2c1b9cfefa85005
vagrant <vagrant@vagrant.vm>
2022-10-07 07:05:49 +0000

    Initial commit

---
+++ jenkins-install/.terraform.lock.hcl
@@ -0,0 +1,41 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/google" {
+  version     = "4.34.0"
+  constraints = "4.34.0"
+  hashes = [
+    "h1:ffds+Dg9PFD2bjzHbfjhvQkVk9tckCbHiiqUERlSAgY=",
+    "zh:1319b4e17e3242f36338ab4f422c09183dcae320272f611cfd06a225a2c8af8f",
+    "zh:29ca012946e89998af0924b64e482a904c29abba0c773930ff72b92d82bb740c",
+    "zh:3f87b2063a50c38482395e5dadd4bb4b451ac2f4b62387b07ef9e0fe54888e73",
+    "zh:55cef4dce0f563b853975b253861f46464ce25c1d5c52b930824d8cc65e3d882",
+    "zh:653cc915e79ccad55dfd78c98b0f9835996ce70d2415fc5ab8a3f0b5e2a3e2c9",
+    "zh:6be45154d6c190b4f37d7d5a464c900c1aee3d86806e2c3610f6c5e2e9e4230a",
+    "zh:895e5742fe4da10470c54441bec173fc1bb3f8e4123e1b12154175e12a0ad711",
+    "zh:a287505e82a55db481fc5da97c9d6a6bd2e8ba47e6688106ae299db68f3005d5",
+    "zh:d627b3b9c73e71bb109c5ce9b50dfe1f7a03e8ca08c9f5c7c68597dd588f1a07",
+    "zh:dcb0b653c479dcadc9d3403a784074d8f7e9a6f5bba633a11e4a1852ded2b3a8",
+    "zh:ef738a6ef05ef748074e05e516adb21a669c744808d47d0638bc01d85d26bcae",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/tls" {
+  version = "4.0.3"
+  hashes = [
+    "h1:TtxH+PuCVPoWUpkMhjdz4QB1i7A4/Ag9kIouPabKDrI=",
+    "zh:0b34a21c535db27a71d1b76a635352ad5dc31d81cdee3d34926629c8919990a1",
+    "zh:160008f7a89ec99f1a79d05bd9c2f11679edbcac5ecf43f618d0a260d003d5ce",
+    "zh:2b951a9c75be26ff320d979c121832866db75f08bcbe326cf5290c8497e345a4",
+    "zh:5032695a3fb47914fa6037f21e7f06d0b684ffddde0c758477a63008476e97d0",
+    "zh:581f518e5104e5bdbdb9f5af35628dcb04a6fee11c6279e4ac1480d7a2bde39f",
+    "zh:66f36f9da00e7a39952e9e4cf158b552a98d8afabbb74804bf27b872b4fdefc6",
+    "zh:a159589b44d353376f86d05f1819952445f9a1c00a3bcc2745217d6f513f5147",
+    "zh:de077f6b852c8d637dbd9b9542ffd590db3d96f9f718acee6645ac6e22365b3c",
+    "zh:e0ea922619aea6b97f416fd8c129af944df9f37b81417ae7b970c6895498843e",
+    "zh:e7705f4f35332f598e6d4078c7e7e734e4728c5ec8642a50672e29d51d8f3be3",
+    "zh:eb64e7a5ebd37b8f59acc91e033897a5d454998f70e57047c94dbfb649503320",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
---
+++ jenkins-install/main.tf
@@ -0,0 +1,75 @@
+resource "google_compute_network" "vpc_network" {
+  project                 = var.project_id
+  name                    = "vpc-network"
+  auto_create_subnetworks = true
+  mtu                     = 1460
+}
+
+resource "google_compute_instance" "default" {
+  name         = "test-vm1"
+  machine_type = "e2-medium"
+
+  zone = var.zone
+
+  tags = ["demo-vm-instance"]
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1804-lts"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+
+    access_config {
+      nat_ip = google_compute_address.static.address
+    }
+  }
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = var.email
+    scopes = ["cloud-platform"]
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install -y python3-pip python3-dev",
+    ]
+    connection {
+      type        = "ssh"
+      host        = google_compute_address.static.address
+      user        = var.user
+      timeout     = "500s"
+      private_key = file(var.privatekeypath)
+      # private_key = tls_private_key.rsa_4096.private_key_openssh
+    }
+  } 
+  depends_on = [google_compute_firewall.ssh-rule]
+
+ metadata = {
+    ssh-keys = "${var.user}:${file(var.publickeypath)}"
+    # ssh-keys = "${var.user}:${tls_private_key.rsa_4096.public_key_openssh}"
+  }
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ${var.user} -i '${google_compute_address.static.address},' --private-key ${var.privatekeypath} -e 'pub_key=${var.publickeypath}' ./roles/jenkins/tasks/main.yml"
+  }
+}
+resource "google_compute_firewall" "ssh-rule" {
+  name    = "demo-ssh"
+  network = google_compute_network.vpc_network.name
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80", "8080"]
+  }
+  target_tags   = ["demo-vm-instance"]
+  source_ranges = ["0.0.0.0/0"]
+}
+# We create a public IP address for our google compute instance to utilize
+resource "google_compute_address" "static" {
+  name       = "vm-public-address"
+  project    = var.project_id
+  region     = var.region
+  depends_on = [google_compute_firewall.ssh-rule]
+}
