3de186ac9942b249221cf88711faaad90366b36c
Michal Nowacki <mnowack@softserveinc.com>
2022-10-13 11:20:52 +0200

    Initial commit. Managed instance group

---
+++ gcp-006-managed_instance_group/variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  type        = string
+  default     = "europe-central2"
+  description = "europe-central2 region"
+}
+variable "zone" {
+  type        = string
+  default     = "europe-central2-c"
+  description = "europe-central2-c zone"
+}
+variable "project_id" {
+  type = string
+}
+variable "user" {
+  type = string
+}
+variable "email" {
+  type = string
+}
